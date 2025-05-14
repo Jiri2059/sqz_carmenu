048431d3b417f2b3246d7b2aa34253eadde66d0a
Jiricech2059 <78664588+Jiri2059@users.noreply.github.com>
2025-05-14 15:52:05 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,8 @@
 Config = {}
-Config.Locale                     = 'en' -- Locale used in config
+Config.Locale = 'en' -- Locale used in config
+Config.Framework = 'esx' -- esx/qb
 Config.FrontCruiseSpeedControl = 11 -- Key to set front speed crusier on
 Config.CruiserControl = 10 -- Key to set Crusier On
 Config.OpenCarMenu = 344 -- Key to open Main (control menu)
-Config.minimalCrusierSpeed = 10 -- Minimal speed (in kmh) to turn Cruiser On
+Config.MinimalCrusierSpeed = 10 -- Minimal speed (in kmh) to turn Cruiser On
+Config.Menu = 'ox_context' -- ox_context: ox_lib menu with mouse control, ox_menu: ox_lib menu with arrows (keyboard) control, ox_radial: ox_lib radial menu
