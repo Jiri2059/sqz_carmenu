2306a78abdbc24092c056868758822f537472d8d
Jiricech2059 <78664588+Jiri2059@users.noreply.github.com>
2025-05-14 15:48:03 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,19 @@ fx_version 'adamant'
 
 game 'gta5'
 
+author 'czsquizer'
 description 'SQZ Car Control'
 
-version '1.0.3'
+version '1.0.4'
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'config.lua',
 	'client/main.lua',
 	'locales/cs.lua',
 	'locales/en.lua'
 }
 
-server_scripts {
+shared_scripts {
+	'@ox_lib/init.lua',
 	'config.lua'
 }
