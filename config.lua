5dd5d0f43e3d8fd53279358a9083ed96c8e9b78c
Jiricech2059 <jiricech2059@gmail.com>
2025-05-23 16:26:54 +0200

    Update config.lua to enhance menu configuration and improve comments

--- config.lua
+++ config.lua
@@ -1,18 +1,21 @@
 Config = {}
-Config.Locale = 'en' -- Locale used in config
-Config.Framework = 'esx' -- esx/qb
+Config.Locale = 'en' -- Locale (cs/de/en/es/fr/it/pl)
+-- Config.Framework = 'esx' -- esx/qb
 
-Config.Menu = 'ox_context' -- ox_context: ox_lib menu with mouse control, ox_menu: ox_lib menu with arrows (keyboard) control, ox_radial: ox_lib radial menu
+Config.Menu = 'ox_radial' -- ox_context: ox_lib menu with mouse control, ox_menu: ox_lib menu with arrows (keyboard) control, ox_radial: ox_lib radial menu
+Config.Notifications = 'ox' -- ox: ox_lib notifications, esx: esx notifications, pnotify: pNotify, cd: cd notifications, mythic: mythic notifications
+Config.ox_menuPosition = 'top-right' -- ox_menu position (top-left, top-right, bottom-left, bottom-right)
 
 -- Car Menu
 Config.OpenCarMenu = 'F5' -- Key to open Menu
 Config.MenuItems = { -- Menu options display (set to false to hide from menu)
     EngineToggle = true, -- Toggle vehicle engine (on/off)
+    NeonLightsToggle = true, -- Toggle vehicle neons (on/off)
+    Extras = true -- Add/Remove vehicle extras
+    Liveries = true -- Change vehicle livery
     Doors = true, -- Open/Close doors
     Windows = true, -- Open/Close windows
-    NeonLightsToggle = true, -- Toggle vehicle neons (on/off)
-    LightsToggle = true, -- Toggle vehicle lights (on/off)
-    Extras = true -- Add/Remove vehicle extras
+    Lights = true, -- Toggle vehicle lights (on/off)
 }
 
 -- Cruiser
