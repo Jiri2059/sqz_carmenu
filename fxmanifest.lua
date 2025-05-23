8b7bee4f47fd6f161cfe6457f9765889a63497e4
Jiricech2059 <jiricech2059@gmail.com>
2025-05-23 16:27:01 +0200

    Refactor fxmanifest.lua to include lua54 support and clean up shared_scripts section

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,24 @@
 fx_version 'adamant'
-
 game 'gta5'
+lua54 'yes'
 
 author 'czsquizer'
 description 'SQZ Car Control'
 
 version '1.0.4'
 
+-- shared_scripts {
+-- 	'@ox_lib/init.lua', -- Remove if you do not use ox_lib
+-- 	'config.lua',
+-- 	'locale.lua',
+--     'locales/*.lua'
+-- }
+
 client_scripts {
 	'@es_extended/locale.lua',
-	'client/main.lua',
-	'locales/cs.lua',
-	'locales/en.lua'
-}
-
-shared_scripts {
-	'@ox_lib/init.lua',
-	'config.lua'
+	'@ox_lib/init.lua', -- Remove if you do not use ox_lib
+	'config.lua',
+	'locale.lua',
+    'locales/*.lua',
+	'client/*.lua',
 }
