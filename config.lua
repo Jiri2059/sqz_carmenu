3e76d763a8f3026f626b7d9e4a67080ed4f457eb
Jiricech2059 <jiricech2059@gmail.com>
2025-05-24 14:57:15 +0200

    Update configuration for notifications and framework settings

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 Config = {}
 Config.Locale = 'en' -- Locale (cs/de/en/es/fr/it/pl)
--- Config.Framework = 'esx' -- esx/qb
+Config.Framework = 'esx' -- esx/qb
 
 Config.Menu = 'ox_radial' -- ox_context: ox_lib menu with mouse control, ox_menu: ox_lib menu with arrows (keyboard) control, ox_radial: ox_lib radial menu
-Config.Notifications = 'ox' -- ox: ox_lib notifications, esx: esx notifications, pnotify: pNotify, cd: cd notifications, mythic: mythic notifications
+Config.Notifications = 'ox' -- ox: ox_lib notifications, esx: esx notifications, qb: qb notifications, pnotify: pNotify, cd: cd notifications, mythic: mythic notifications
 Config.ox_menuPosition = 'top-right' -- ox_menu position (top-left, top-right, bottom-left, bottom-right)
 
 -- Car Menu
