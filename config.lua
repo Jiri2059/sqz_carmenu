cb0770cbabb818fc86b1143aed02e6c09ce95055
Jiricech2059 <jiricech2059@gmail.com>
2025-05-25 20:22:45 +0200

    Fix menu configuration and correct syntax in config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 Config.Locale = 'en' -- Locale (cs/de/en/es/fr/it/pl)
 Config.Framework = 'esx' -- esx/qb
 
-Config.Menu = 'ox_radial' -- ox_context: ox_lib menu with mouse control, ox_menu: ox_lib menu with arrows (keyboard) control, ox_radial: ox_lib radial menu
+Config.Menu = 'ox_context' -- ox_context: ox_lib menu with mouse control, ox_menu: ox_lib menu with arrows (keyboard) control, ox_radial: ox_lib radial menu
 Config.Notifications = 'ox' -- ox: ox_lib notifications, esx: esx notifications, qb: qb notifications, pnotify: pNotify, cd: cd notifications, mythic: mythic notifications
 Config.ox_menuPosition = 'top-right' -- ox_menu position (top-left, top-right, bottom-left, bottom-right)
 
@@ -11,8 +11,8 @@ Config.OpenCarMenu = 'F5' -- Key to open Menu
 Config.MenuItems = { -- Menu options display (set to false to hide from menu)
     EngineToggle = true, -- Toggle vehicle engine (on/off)
     NeonLightsToggle = true, -- Toggle vehicle neons (on/off)
-    Extras = true -- Add/Remove vehicle extras
-    Liveries = true -- Change vehicle livery
+    Extras = true, -- Add/Remove vehicle extras
+    Liveries = true, -- Change vehicle livery
     Doors = true, -- Open/Close doors
     Windows = true, -- Open/Close windows
     Lights = true, -- Toggle vehicle lights (on/off)
